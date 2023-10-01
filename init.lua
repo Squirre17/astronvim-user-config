f927cc0a28f9520ab1d543aa01da95678e0b9dbc
squirre17 <3319277663@qq.com>
2023-10-01 17:57:36 +0800

    complete plugin osc52

--- init.lua
+++ init.lua
@@ -15,35 +15,6 @@ end
 -- 调用函数
 addCscopeFile()
 
--- local function clipboard_yank()
---     vim.api.nvim_command("echom clipboard_yank")
---     vim.fn.system('xclip -i -selection clipboard', vim.fn.getreg('"'))
--- end
---
--- local function clipboard_paste()
---     vim.api.nvim_command("echom clipboard_paste")
---     vim.fn.setreg('"', vim.fn.system('xclip -o -selection clipboard'))
---     -- vim.cmd('normal! p')
--- end
---
--- vim.api.nvim_set_keymap('v', 'y', ':<C-u>lua clipboard_yank()<CR>', { silent = true })
--- vim.api.nvim_set_keymap('v', 'd', ':<C-u>lua clipboard_yank()<CR>', { silent = true })
--- vim.api.nvim_set_keymap('n', 'p', ':<C-u>lua clipboard_paste()<CR>p', { silent = true })
-
-
--- vim.g.clipboard = {
---     name = 'WslClipboard',
---     copy = {
---         ['+'] = 'clip.exe',
---         ['*'] = 'clip.exe',
---     },
---     paste = {
---         ['+'] = 'vim.fn.system("powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace(\"`r\", \"\"))")',
---         ['*'] = 'vim.fn.system("powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace(\"`r\", \"\"))")',
---     },
---     cache_enabled = 0,
--- }
-
 -- set plugin "ojroques/nvim-osc52"
 
 return {
---
+++ plugins/init.lua
@@ -0,0 +1,10 @@
+-- sync by :PackerSync
+return {
+  {
+    "ojroques/nvim-osc52",
+    name = "osc52",
+    config = function()
+      require("osc52").setup {}
+    end,
+  },
+}
